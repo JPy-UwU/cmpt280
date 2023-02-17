e75cde51cb836c18921dc148757b5b59fc818d17
JP <jay.patel.py@gmail.com>
2023-02-17 10:29:00 -0600

    Added a skill tree in SkillTree - added main method with a skill tree inside - skill tree is based on One Piece

--- asn3/src/SkillTree.java
+++ asn3/src/SkillTree.java
@@ -24,7 +24,32 @@ public class SkillTree extends BasicMAryTree280<Skill> {
 	public SkillTree rootSubTree(int i) {
 		return (SkillTree)super.rootSubtree(i);
 	}
-	
-	
+
+	public static void main(String[] args) {
+		// The skill tree is based on an anime called `One Piece`
+		BasicMAryTree280<String> opTree = new BasicMAryTree280<String>("One Piece", 3);
+		opTree.setRootSubtree(new BasicMAryTree280<String> ("Combat Skills", 3), 1);
+		opTree.setRootSubtree(new BasicMAryTree280<String>("Devil Fruits", 3), 2);
+		opTree.setRootSubtree(new BasicMAryTree280<String>("Haki", 3), 3);
+
+		BasicMAryTree280<String> combatSkills = opTree.rootSubtree(1);
+		combatSkills.setRootSubtree(new BasicMAryTree280<String>("Hand-to-Hand", 3), 1);
+		combatSkills.setRootSubtree(new BasicMAryTree280<String>("Swordsmanship", 3), 2);
+
+		BasicMAryTree280<String> devilFruits = opTree.rootSubtree(2);
+		devilFruits.setRootSubtree(new BasicMAryTree280<String>("Paramecia", 3), 1);
+		devilFruits.setRootSubtree(new BasicMAryTree280<String>("Zoan", 3), 2);
+		devilFruits.setRootSubtree(new BasicMAryTree280<String>("Logia", 3), 3);
+
+		BasicMAryTree280<String> zoanDevilFruit = devilFruits.rootSubtree(2);
+		zoanDevilFruit.setRootSubtree(new BasicMAryTree280<String>("Mythical Zoan", 1), 1);
+
+		BasicMAryTree280<String> haki = opTree.rootSubtree(3);
+		haki.setRootSubtree(new BasicMAryTree280<String>("Observation", 3), 1);
+		haki.setRootSubtree(new BasicMAryTree280<String>("Armament", 3), 2);
+		haki.setRootSubtree(new BasicMAryTree280<String>("Conqueror's", 3), 3);
+
+		System.out.println(opTree.toString());
+	}
 
 }
